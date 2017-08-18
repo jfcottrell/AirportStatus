f5f2928dd1ba61e445c1ecc701a9bae727f3c3a2
John Cottrell <johncottrell@Johns-iMac.local>
2017-08-18 09:53:25 -0500

    Initial Commit

---
+++ AirportStatusTests/AirportStatusTests.swift
@@ -0,0 +1,36 @@
+//
+//  AirportStatusTests.swift
+//  AirportStatusTests
+//
+//  Created by John Cottrell on 8/18/17.
+//  Copyright © 2017 John Cottrell. All rights reserved.
+//
+
+import XCTest
+@testable import AirportStatus
+
+class AirportStatusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
