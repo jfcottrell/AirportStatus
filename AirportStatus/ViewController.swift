d8944ace55fee48bb65d2be6ebf3bda41fb5a263
John Cottrell <jcottrell@yahoo.com>
2017-10-20 15:07:09 -0500

    first commit

--- AirportStatus/ViewController.swift
+++ AirportStatus/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let urlString: String = "https://services.faa.gov/airport/status/HOU?format=application/json"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let url = URL(string: urlString) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        
+        // set up the session
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        // make the request
+        let task = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            // do stuff with response, data & error here
+            if let error = error {
+                print(error)
+            } else {
+                print("NO ERROR.")
+            }
+            
+            if let response = response {
+                print(response)
+            } else {
+                print("NO RESPONSE.")
+            }
+            
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            } else {
+                print("NO DATA.")
+            }
+            
+        })
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
